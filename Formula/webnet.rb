91181881c9605fc93eb3712dbf7d2626717e2bf9
Kevin-12-gif <kevinalimohsin@gmail.com>
2026-08-01 22:19:53 -0700

    Update Homebrew Formula with full dependencies

--- Formula/webnet.rb
+++ Formula/webnet.rb
@@ -4,18 +4,34 @@ class Webnet < Formula
   desc "Standalone Incognito Web Browser with Chrome identity"
   homepage "https://github.com/Kevin-12-gif/homebrew-webnet"
   url "https://github.com/Kevin-12-gif/homebrew-webnet/archive/refs/heads/main.tar.gz"
-  sha256 "a2c9e580b3e6af05e611420e2916cacc5b49b7de5e886e372fd3154a50671f22"
+  sha256 "846047f36f18b0d5e25e190cb069e6f545401fb359dcbb3c2fa22598dada2a70"
   license "MIT"
   version "1.0.0"
 
   depends_on "node"
 
+  on_linux do
+    depends_on "webkit2gtk"
+    depends_on "gtk+3"
+  end
+
   def install
+    # This installs node_modules (including webviewjs) and the webnet binary into libexec
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+
+    # Creates a symlink in /usr/local/bin or /opt/homebrew/bin
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
+  def caveats
+    <<~EOS
+      WebNet requires a graphical environment to run.
+      On Linux, ensure you have libwebkit2gtk-4.0 or 4.1 installed if not using Homebrew's version.
+    EOS
+  end
+
   test do
+    # Simple check to see if the binary was installed correctly
     system "#{bin}/webnet", "--help"
   end
 end
